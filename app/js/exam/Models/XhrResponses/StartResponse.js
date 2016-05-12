480778fdd0780a9432cba11a8324b0cf859ea133
haestflod <haestflod@gmail.com>
2016-05-12 22:25:49 +0300

    Slight cleanup
    Figured out how to use js/examangular/main finally!
    Added some tsconfig.json stuff like compile on save.

--- app/js/exam/Models/XhrResponses/StartResponse.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=StartResponse.js.map
--- public/examapp/main.js
+++ public/examapp/main.js
@@ -1,7 +1,7 @@
-/// <reference path="../../../node_modules/angular2/typings/es6-promise/es6-promise.d.ts" />
-/// <reference path="../../../node_modules/angular2/typings/es6-collections/es6-collections.d.ts" />
-/// <reference path="../../../node_modules/angular2/typings/browser.d.ts" />
-////// <reference path="../../../node_modules/angular2/src/common/directives/ng_class.d.ts" />
+/// <reference path="../../node_modules/angular2/typings/es6-promise/es6-promise.d.ts" />
+/// <reference path="../../node_modules/angular2/typings/es6-collections/es6-collections.d.ts" />
+/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
+/// <reference path="../../node_modules/angular2/src/common/directives/ng_class.d.ts" />
 System.register(["angular2/platform/browser", "./components/app"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
@@ -15,7 +15,6 @@ System.register(["angular2/platform/browser", "./components/app"], function(expo
                 app_1 = app_1_1;
             }],
         execute: function() {
-            console.log("moo");
             browser_1.bootstrap(app_1.AppComponent);
         }
     }
