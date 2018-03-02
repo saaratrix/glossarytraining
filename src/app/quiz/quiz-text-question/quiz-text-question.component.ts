5cb1e5c2c7c975b571d7a4114270de5ed04aa17f
haestflod <haestflod@gmail.com>
2018-03-02 14:23:46 +0200

    Pressing enter focuses next question when texting
    If you press enter when writing a question it will focus the next input
    field. It will only select the ones that havn't been answered yet.
    Meaning incorrect or unanswered questions.

--- src/app/quiz/quiz-text-question/quiz-text-question.component.ts
+++ src/app/quiz/quiz-text-question/quiz-text-question.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from "@angular/core";
 import { TextQuestion } from "../../shared/models/text-question";
 
 @Component({
@@ -15,6 +15,9 @@ export class QuizTextQuestionComponent implements OnInit {
   @Output()
   public answered: EventEmitter<TextQuestion>;
 
+  @ViewChild('questionInput')
+  private inputElement: ElementRef;
+
   public revealAnswers: boolean;
 
   constructor () {
@@ -22,6 +25,8 @@ export class QuizTextQuestionComponent implements OnInit {
     this.isReviewed = false;
     this.answered = new EventEmitter<TextQuestion>();
     this.revealAnswers = false;
+
+
   }
 
   ngOnInit () {
@@ -37,6 +42,51 @@ export class QuizTextQuestionComponent implements OnInit {
     }
     else {
       return `Käännä "${this.question.question}" suomeksi`;
+    }
+  }
+
+  public tryFocusNext (event: KeyboardEvent) {
+    if (event.which === 13) {
+      const currentInput = this.inputElement.nativeElement as HTMLInputElement;
+
+      const nodeList: NodeList = document.querySelectorAll(".answer-text-input");
+      let nextInput: HTMLInputElement = null;
+
+      let foundInputElement = false;
+
+      // Iterate over all nodelist items with tabIndex === -1
+      // Check if inputElement == currentInput element to set that it has found the input element
+      for (let i = 0; i < nodeList.length; i++) {
+        const inputElement = nodeList.item(i) as HTMLInputElement;
+        // Exclude the this components element
+        if (inputElement === currentInput) {
+          foundInputElement = true;
+          continue;
+        }
+        // Exclude elements that doesn't have a tabIndex,  for example their question is correct!
+        if (inputElement.tabIndex === -1) {
+          continue;
+        }
+
+        // If the foundInputElement is true then the next item we find is the nextInput element
+        // Also break out of the forloop as we've found what we're looking for!
+        if (foundInputElement) {
+          nextInput = inputElement;
+          break;
+        }
+
+        // If null set nextInput as first found so if there are no elements after the inputElement it'll go circle back to the first elements.
+        if (!nextInput) {
+          nextInput = inputElement;
+        }
+      }
+      // If no nextInput was found then there is no element to focus!
+      if (!nextInput) {
+        return;
+      }
+
+      event.preventDefault();
+      nextInput.focus();
     }
   }
 
